30cddd82b797c510ac33e1de57589088af1e075a
tomasz.lis <tomasz.lis@comarch.pl>
2020-01-17 15:53:57 +0100

    Created audio stream service

---
+++ Microservices/Stream/Stream.Infrastructure/Services/YtAudioStreamService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.NodeServices;
+using Stream.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stream.Infrastructure.Services
+{
+    public class YtAudioStreamService : IYtAudioStreamService
+    {
+        protected INodeServices NodeServices { get; set; }
+
+        public YtAudioStreamService(INodeServices nodeServices)
+        {
+            NodeServices = nodeServices;
+        }
+
+        public System.IO.Stream GetAudioStream(string videoId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
